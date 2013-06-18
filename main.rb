82e7eb8607d93e2fa2776fccf3036edbcab8f166
earwerm <michael@earwerm.com>
2013-06-18 15:33:18 +1000

    Add Calcit options

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ puts "Choose Calulator"
 
 puts "1 simple"
 puts "2 advanced"
+puts "Q quit"
 
 user_input = gets.chomp
 
@@ -12,6 +13,18 @@ if user_input == '1'
 	puts "d division"
 
 	user_input = gets.chomp
+
+elsif user_input == '2'
+	puts "e exponential"
+	puts "p to the power"
+	puts "sr square root"
+
+	user_input = gets.chomp
+
+elsif user_input == 'Q'
+    puts "Thank you, come again!"
+
+    user_input = gets.chomp
 end
 
 
