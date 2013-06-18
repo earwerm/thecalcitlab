375a15bd7e6b283348f0aa4258ef27fe3dfe6314
Daniel T <tsui.daniel@gmail.com>
2013-06-18 15:19:45 +1000

    add selection menus

--- main.rb
+++ main.rb
@@ -1,1 +1,18 @@
 puts "Choose Calulator"
+
+puts "1 simple"
+puts "2 advanced"
+
+user_input = gets.chomp
+
+if user_input == '1'
+	puts "a addition"
+	puts "m multiplication"
+	puts "s subtraction"
+	puts "d division"
+
+	user_input = gets.chomp
+end
+
+
+
